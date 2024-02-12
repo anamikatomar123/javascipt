9454adc5bd7e14716808f2f13c3f77384507bd32
anamikakacher <anamikakacher171@gmail.com>
2024-02-12 19:49:55 +0530

    added

---
+++ day1.js
@@ -0,0 +1,67 @@
+console.log("welcome to minskole")
+let balance = 0
+console.log(balance)
+balance = 100
+console.log(balance)
+
+
+const  pi = 3.14
+console.log(pi)
+
+let q = 8 
+let r = 4 
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+let s = 10
+let t = 5
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+
+Calculator(12,3)
+Calculator(8,4)
+Calculator(6,3)
+
+
+
+
+function additionA(){
+    console.log(9+9)
+}
+additionA()
+additionA()
+additionA()
+additionA()
+
+
+function additionB(x,y){
+    console.log(x+y)
+}
+additionB(12,3)
+additionB(6,3)
+
+
+function additionC(x,y){
+    return x + y
+}
+let q1 = additionC(13,4)
+console.log(q1)
+
+    
