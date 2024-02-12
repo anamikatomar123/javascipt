5d6e9e7cb782ad1431c94f6866ce054cf99b35c0
anamikakacher <anamikakacher171@gmail.com>
2024-02-12 19:51:18 +0530

    added

---
+++ day2.js
@@ -0,0 +1,47 @@
+
+
+let a = 10 
+console.log(a)
+console.log(typeof a)
+
+let b  = 90.9
+console.log(typeof b)
+
+let k = true
+console.log(k)
+console.log(typeof k)
+
+let first_name = "chinmay"
+console.log(first_name)
+console.log(typeof first_name)
+
+console.log(typeof 10) 
+console.log(typeof '10')
+
+
+console.log(2 === '2')
+console.log(2 !== '2')
+console.log(2 === 2)
+console.log(2 !== 2)
+console.log(2 == '2')
+console.log(2 != '2')
+console.log(2 != 2)
+console.log(7 > 3)
+console.log(7 < 3)
+console.log(7 >= 3)
+console.log(7 <= 3)
+console.log(7 >= 7)
+console.log(7 <= 7)
+
+
+console.log(7 == 7 && 8 === 8)
+console.log(6 === '6' && 7 === 7)
+console.log(6 === 6  && 7 !== 7)
+console.log(5 == 4 && 8 == 9)
+
+
+console.log(7 == 7 || 8 === 8)
+console.log(6 === '6' || 7 === 7)
+console.log(6 === 6  || 7 !== 7)
+console.log(5 == 4 || 8 == 9)
+
